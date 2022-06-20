9c2be3f904c24f47c8b0a04b7c9b3c18ff723ce1
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-06-20 14:02:33 +0300

    feat:Добавил middleware cors и express.json

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const sequelize = require("./db");
 const models = require("./models/models");
 
-const app = express();
-
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Проверяю что все работает
+app.get("/", (request, response) => {
+  try {
+    response.status(200).json({ message: "Все прекрасно работает" });
+  } catch (error) {
+    response.status(500).json(error.message);
+  }
+});
 
 const start = async () => {
   try {
