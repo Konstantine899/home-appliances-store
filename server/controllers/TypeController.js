ca022ca8b2398468c8aea334ee3364b8b018c953
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-06-21 18:22:38 +0300

    feat:TypeController функция create и getAll

--- server/controllers/TypeController.js
+++ server/controllers/TypeController.js
@@ -1,6 +1,24 @@
+const { Type } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
 class TypeController {
-  async create(request, response) {}
-  async getAll(request, response) {}
+  async create(request, response, next) {
+    try {
+      const { name } = request.body;
+      const type = await Type.create({ name });
+      return response.json(type);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+  async getAll(request, response, next) {
+    try {
+      const types = await Type.findAll();
+      return response.json(types);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
 }
 
 module.exports = new TypeController();
