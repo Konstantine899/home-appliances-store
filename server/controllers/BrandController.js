b6c323697a69ca3b78e5ff59f122cc2cf1384827
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-06-21 18:45:24 +0300

    feat:BrandController функция create и getAll

--- server/controllers/BrandController.js
+++ server/controllers/BrandController.js
@@ -1,6 +1,24 @@
+const { Brand } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
 class BrandController {
-  async create(request, response) {}
-  async getAll(request, response) {}
+  async create(request, response, next) {
+    try {
+      const { name } = request.body;
+      const brand = await Brand.create({ name });
+      return response.json(brand);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+  async getAll(request, response, next) {
+    try {
+      const brands = await Brand.findAll();
+      return response.json(brands);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
 }
 
 module.exports = new BrandController();
